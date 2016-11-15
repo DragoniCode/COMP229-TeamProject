2bab0131dc3bcacbe2a68ef023757578e7c2ef48
DragoniCode <DragoniCode@users.noreply.github.com>
2016-11-15 00:41:42 -0500

    Initial commit.

---
+++ COMP229-TeamProject/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(COMP229_TeamProject.Startup))]
+namespace COMP229_TeamProject
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
